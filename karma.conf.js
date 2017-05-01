2a48b48aa81a4106641c1a53e7d24fb87cf952ff
Jérémy Lal <kapouer@melix.org>
2017-05-01 18:48:58 +0200

    Unleash some tests and concurrency is up to 5 with OSS plan

--- karma.conf.js
+++ karma.conf.js
@@ -3,14 +3,13 @@
 var customLaunchers = {
   // You can use this tool to generate the correct config:
   // https://wiki.saucelabs.com/display/DOCS/Platform+Configurator#/
-  //win10chrome: { base: "SauceLabs", browserName: "chrome", platform: "Windows 10" },
-  //androidChrome: { base: "SauceLabs", browserName: "android", platform: "Linux" },
-  //win10firefox: { base: "SauceLabs", browserName: "firefox", platform: "Windows 10" },
-  //iosSafari: { base: "SauceLabs", browserName: "iphone", platform: "OS X 10.10" },
-  //iosSafari92: { base: "SauceLabs", browserName: "iphone", platform: "OS X 10.10", version: "9.2" },
-  //win10ie11: { base: "SauceLabs", browserName: "internet explorer", platform: "Windows 10" },
-  //win7ie9: { base: "SauceLabs", browserName: "internet explorer", platform: "Windows 7", version: "9.0" },
-  linuxChrome: { base: "SauceLabs", browserName: "chrome", platform: "Linux" }
+  win10chrome: { base: "SauceLabs", browserName: "chrome", platform: "Windows 10" },
+  // androidChrome: { base: "SauceLabs", browserName: "android", platform: "Linux" },
+  win10firefox: { base: "SauceLabs", browserName: "firefox", platform: "Windows 10" },
+  iosSafari: { base: "SauceLabs", browserName: "iphone", platform: "OS X 10.10" },
+  // iosSafari92: { base: "SauceLabs", browserName: "iphone", platform: "OS X 10.10", version: "9.2" },
+  win10ie11: { base: "SauceLabs", browserName: "internet explorer", platform: "Windows 10" },
+  win7ie9: { base: "SauceLabs", browserName: "internet explorer", platform: "Windows 7", version: "9.0" }
 };
 
 module.exports = function(config) {
@@ -32,7 +31,7 @@ module.exports = function(config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    concurrency: 1,
+    concurrency: 5,
     client: {
       captureConsole: true,
       timeout: 20000
